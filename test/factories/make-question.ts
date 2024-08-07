9128b9da533f782525bc9e274bad7fbfa42e7903
pedro <pfaria.sjp@ecori.com.br>
2024-08-07 16:20:42 -0300

    instalando faker js e configurando make question

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,16 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityID } from '@/core/entities/unique-entity'
 import {
   Questions,
   QuestionsProps,
 } from '@/domain/forum/enterprise/entities/questions'
 
-export function makeQuestion(override: Partial<QuestionsProps> = {}) {
-  const createQuestion = Questions.create({
-    authorId: new UniqueEntityID(),
-    title: 'example question',
-    content: 'question slug',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionsProps> = {},
+  id?: UniqueEntityID,
+) {
+  const createQuestion = Questions.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return createQuestion
 }
